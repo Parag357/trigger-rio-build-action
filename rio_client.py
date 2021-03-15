e2712c29a592b2c411e916ccf739c5aa1cb6c0db
Parag Bhattacharjee <parag.bhattacharjee@rapyuta-robotics.com>
2021-03-15 12:24:24 +0530

    updating trigger action

---
+++ rio_client.py
@@ -0,0 +1,15 @@
+import os
+from rapyuta_io import Client
+
+
+def get_rio_client():
+    client = None
+    auth_token = os.getenv('AUTH_TOKEN', default=None)
+    project_id = os.getenv('PROJECT_ID', default=None)
+    if not (auth_token and project_id):
+        return client
+    client = Client(auth_token=auth_token, project=project_id)
+    return client
+
+
+
